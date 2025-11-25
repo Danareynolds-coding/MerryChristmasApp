e1bbb5eec148eaf328b96dbf4436addd620e0640
Danareynolds-coding <dana.reynolds@mscoding.org>
2025-11-24 18:25:08 -0600

    first commit

---
+++ routes/router.js
@@ -0,0 +1,20 @@
+// // ('programschristmasdb api')//
+// const express = require('express')
+// const router = express.Router()
+// const PORT = process.env.PORT || 3000
+
+// // rootroute http://localhost:3000/api
+// router.get('/api', (req, res)=> {
+//   res.json({
+//   'All Programs':`http://localhost:${POST}/api/programs`
+//   })
+// })
+
+// router.use('/api/programs', require('./api/programsRoutes'))
+
+// // error handling
+// router.use((req, res, next)=> {
+//   res.status(404)
+//   .send('<h1>404 Error This page does not exist!</h1>')
+// })
+// module.exports = router
